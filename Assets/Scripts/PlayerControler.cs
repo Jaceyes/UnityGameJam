bc5435e5ae8f11d4549d2b787b951f1ff0685190
Guitar <squirreltail@yeah.net>
2024-12-22 21:37:31 +0800

    背景无缝衔接

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class PlayerControler : MonoBehaviour
 {
     [SerializeField]private Transform background1;
-    [SerializeField]private Transform[] background1Array;
+    [SerializeField]private List<Transform> background1Array;
     [SerializeField]private Transform background2;
-    [SerializeField]private Transform[] background2Array;
+    [SerializeField]private List<Transform> background2Array;
     [SerializeField]private Transform background3;
-    [SerializeField]private Transform[] background3Array;
+    [SerializeField]private List<Transform> background3Array;
     [SerializeField]private Transform background4;
-    [SerializeField]private Transform[] background4Array;
+    [SerializeField]private List<Transform> background4Array;
     [SerializeField]private Transform background5;
-    [SerializeField]private Transform[] background5Array;
+    [SerializeField]private List<Transform> background5Array;
     [SerializeField]private Transform ground;
 
     [SerializeField]private float moveSpeed = 5f;
+
+    private float rightBorder;
+
+    private void Start(){
+        rightBorder = Camera.main.ViewportToWorldPoint(new Vector2(1, 0)).x;
+    }
 
     private void Update(){
         if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.Mouse0)){
@@ -35,6 +41,30 @@ public class PlayerControler : MonoBehaviour
     }
 
     private void BackgroundControl(){
-        
+        if(background1Array[background1Array.Count-2].position.x<= transform.position.x){
+            AddBackground(background1Array);
+        }
+        if(background2Array[background2Array.Count-2].position.x<= transform.position.x){
+            AddBackground(background2Array);
+        }
+        if(background3Array[background3Array.Count-2].position.x<= transform.position.x){
+            AddBackground(background3Array);
+        }
+        if(background4Array[background4Array.Count-2].position.x<= transform.position.x){
+            AddBackground(background4Array);
+        }
+        if(background5Array[background5Array.Count-2].position.x<= transform.position.x){
+            AddBackground(background5Array);
+        }
+    }
+
+    private void AddBackground(List<Transform> list){
+        if (list.Count > 1)
+        {
+            Transform firstElement = list[0];
+            firstElement.position = new Vector3(list[list.Count - 1].position.x + list[0].GetComponent<SpriteRenderer>().bounds.size.x, list[0].position.y, list[0].position.z);
+            list.RemoveAt(0);
+            list.Add(firstElement);
+        }
     }
 }
